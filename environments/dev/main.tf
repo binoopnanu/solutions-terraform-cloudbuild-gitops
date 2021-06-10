f6444b7f6760e5b90a4b2d7c1303119a28de2436
Bin Nan <binoop.nanu@pryzm.health>
2021-06-10 22:00:14 +1000

    Update project IDs and buckets

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -22,20 +22,9 @@ provider "google" {
 }
 
 resource "google_storage_bucket" "static-site" {
-  name          = "istio-image-store.com"
+  name          = "istio-image-store-koya"
   location      = "EU"
   force_destroy = true
 
   uniform_bucket_level_access = true
-
-  website {
-    main_page_suffix = "index.html"
-    not_found_page   = "404.html"
-  }
-  cors {
-    origin          = ["http://image-store.com"]
-    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
-    response_header = ["*"]
-    max_age_seconds = 3600
-  }
 }
